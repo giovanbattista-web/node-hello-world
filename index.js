e105796edb7da4277c944a7886a468bdb2401335
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-05-04 10:21:25 +0200

    [UPD] CORREZIONE DEL PROFESSORE

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // console.log('Hello Boolean');
 
-const names = process.argv[2];
-console.log(names);
+// BONUS // 
+// PASSARE DA TERMINARE UNA FRASE DIVERSA DALLA PRECEDENTE, IN PIU' UN CONTROLLO SUL VALORE PASSATO CHE NON SIA UNDEFINED,
+//  SE DOVESSE ESSERE INDEFINITO RESTITUISCE LA STRINGA Boolean 
+const param = process.argv[2] ? process.argv[2] : 'Boolean';
+console.log(`Hello ${param}`);
