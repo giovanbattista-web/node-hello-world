334defebf54e8655d004670076ec022108e8f162
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-09-19 07:14:52 +0200

    [UPD] ULTERIORE OTTIMIZZAZIONE EFFETTUATA

---
+++ bonus/index1.js
@@ -0,0 +1,6 @@
+console.log('Hello Boolean');
+
+// PASSARE DA TERMINARE UNA FRASE DIVERSA DALLA PRECEDENTE, IN PIU' UN CONTROLLO SUL VALORE PASSATO CHE NON SIA UNDEFINED,
+//  SE DOVESSE ESSERE INDEFINITO RESTITUISCE LA STRINGA Boolean 
+const param = process.argv[2] ? process.argv[2] : 'Boolean';
+console.log(`Hello ${param}`);
--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 console.log('Hello Boolean');
-
-// BONUS // 
-// PASSARE DA TERMINARE UNA FRASE DIVERSA DALLA PRECEDENTE, IN PIU' UN CONTROLLO SUL VALORE PASSATO CHE NON SIA UNDEFINED,
-//  SE DOVESSE ESSERE INDEFINITO RESTITUISCE LA STRINGA Boolean 
-const param = process.argv[2] ? process.argv[2] : 'Boolean';
-console.log(`Hello ${param}`);
